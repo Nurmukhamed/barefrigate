85547518021226fde22b9fa92800767ada21132c
Nurmukhamed Artykaly <nurmukhamed.artykaly@hdfilm.kz>
2026-01-14 20:47:45 +0500

    fixed errors after deploy.

--- templates/frigate/pre-frigate.sh
+++ templates/frigate/pre-frigate.sh
@@ -4,23 +4,9 @@
 
 set -o errexit -o nounset -o pipefail
 
-export NVIDIA_VISIBLE_DEVICES="all"
-export NVIDIA_DRIVER_CAPABILITIES="compute,video,utility"
-export TOKENIZERS_PARALLELISM="true"
-export TRANSFORMERS_NO_ADVISORY_WARNINGS="1"
-export OPENCV_FFMPEG_LOGLEVEL="8"
-export PYTHONWARNINGS="ignore:::numpy.core.getlimits"
-export HAILORT_LOGGER_PATH="NONE"
-export TF_CPP_MIN_LOG_LEVEL="3"
-export TF_ENABLE_ONEDNN_OPTS="0"
-export AUTOGRAPH_VERBOSITY="0"
-export GLOG_minloglevel="3"
-export GLOG_logtostderr="0"
-export PATH="/usr/local/go2rtc/bin:/usr/local/tempio/bin:/usr/local/nginx/sbin:/command:/package:${PATH}"
-export DEFAULT_FFMPEG_VERSION="7.0"
-export INCLUDED_FFMPEG_VERSIONS="7.0:5.0"
-export S6_CMD_WAIT_FOR_SERVICES_MAXTIME="0"
-export S6_LOGGING_SCRIPT="T 1 n0 s10000000 T"
+{% for item in envs %}
+export {{ item.name }}="{{ item.value }}"
+{% endfor %}
 
 # opt out of openvino telemetry
 if [ -e /usr/local/bin/opt_in_out ]; then
